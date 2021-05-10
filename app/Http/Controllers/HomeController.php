8da2ed585801354469fe684dafc3830992c340d9
keitaChimori <keitachimori.1220@gmail.com>
2021-05-10 23:17:49 +0900

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Memo;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    // ホーム画面表示
+    public function index()
+    {
+        // 全メモを取得
+        $user = Auth::user();
+        $memos = Memo::where('user_id',$user['id'])->where('deleted_at',null)->orderBy('updated_at','DESC')->get();
+        // dd($memos);
+        return view('home',compact('memos','user'));
+    }
+
+    // メモ新規登録画面表示
+    public function create()
+    {
+        // ログイン中のユーザー情報を取得
+        $user = Auth::user();
+        // dd($user); 
+        // 全メモを取得
+        $memos = Memo::where('user_id',$user['id'])->where('deleted_at',null)->orderBy('updated_at','DESC')->get();
+        return view('create',compact('user','memos'));
+    }
+
+    // メモ新規登録実行
+    public function store(Request $request)
+    {
+        $memodata = $request->all();
+        // dd($memodata);
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+
+        $memo_id = Memo::insertGetId([
+            'content' => $memodata['content'],
+            'user_id' => $memodata['user_id'],
+            'status' => "1",
+        ]);
+        
+        // リダイレクト処理
+        return redirect()->route('home');
+    }
+
+    // メモ編集
+    public function edit($id)
+    {
+        $user = Auth::user();
+        // 編集するメモデータを取得
+        $memo = Memo::where('deleted_at',null)->where('user_id',$user['id'])->where('id',$id)->first();
+        // dd($memo);
+        // 全メモを取得
+        $memos = Memo::where('user_id',$user['id'])->where('deleted_at',null)->orderBy('updated_at','DESC')->get();
+        return view('edit',compact('memo','user','memos'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// ホーム画面表示
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+// メモ新規投稿画面表示
+Route::get('/create', [HomeController::class, 'create'])->name('create');
+// メモ新規投稿実行
+Route::post('/store', [HomeController::class, 'store'])->name('store');
+// メモ編集表示
+Route::get('/edit/{id}', [HomeController::class, 'edit'])->name('edit');
